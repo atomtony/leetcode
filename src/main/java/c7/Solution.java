e6798fd45df51c83b90b637b445014117920ace5
jihaitao <jht1264044626@gmail.com>
2020-12-25 16:42:02 +0800

    c7

---
+++ src/main/java/c7/Solution.java
@@ -0,0 +1,16 @@
+package c7;
+
+class Solution {
+    public int reverse(int x) {
+        long n = 0;
+        while (x != 0) {
+            n = n * 10 + x % 10;
+            x = x / 10;
+        }
+        return (int) n == n ? (int) n : 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().reverse(1534236469));
+    }
+}
