edc3252dc0c5196779df41d7ac3eb8537c8692c9
jihaitao <jht1264044626@gmail.com>
2021-01-07 10:10:17 +0800

    Kahn 算法，依赖顺序输出

--- src/main/java/Ch43.java
+++ src/main/java/Ch43.java
@@ -18,40 +18,63 @@ public class Ch43 {
             adj[s].add(t);
         }
 
+        // Kahn 算法
         public void topoSortByKahn() {
             int[] inDegree = new int[v];//统计每个顶点的入度
             for (int i = 0; i < v; i++) {
                 for (int j = 0; j < adj[i].size(); j++) {
+                    // 连接顶点
                     int w = adj[i].get(j);
+                    // 连接顶点入度加1
                     inDegree[w]++;
                 }
             }
 
+            // 记录起始顶点
             LinkedList<Integer> queue = new LinkedList<>();
             for (int i = 0; i < v; i++) {
                 if (inDegree[i] == 0) {
+                    // 入度为0,属于起始顶点
                     queue.add(i);
                 }
             }
-        }
 
+            while (!queue.isEmpty()) {
+                // 起始顶点
+                int i = queue.remove();
+                System.out.print("->" + i);
+
+                // 遍历起始顶点连接的其他顶点
+                for (int j = 0; j < adj[i].size(); j++) {
+                    // 连接点
+                    int k = adj[i].get(j);
+                    // 起始点已经输出，连接点入度减1
+                    inDegree[k]--;
+                    // 连接点入度为0,则变成了起始顶点
+                    if (inDegree[k] == 0) {
+                        queue.add(k);
+                    }
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
-        Graph graph = new Graph(8);
-        graph.addEdge(0, 1);
-        graph.addEdge(0, 3);
 
-        graph.addEdge(1, 2);
-        graph.addEdge(1, 4);
-        graph.addEdge(3, 4);
+        // 0->2->4
+        // 1->3->5
+        // 6->3
 
-        graph.addEdge(2, 5);
-        graph.addEdge(4, 5);
-        graph.addEdge(4, 6);
+        Graph graph = new Graph(7);
+        graph.addEdge(0, 2);
+        graph.addEdge(2, 4);
 
-        graph.addEdge(6, 7);
-        graph.addEdge(5, 7);
+        graph.addEdge(1, 3);
+        graph.addEdge(3, 5);
+
+        graph.addEdge(6, 3);
+
+        graph.topoSortByKahn();
 
     }
 
