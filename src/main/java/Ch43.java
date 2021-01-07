d5d7467768c8686054350281e618e0bb8bc8deae
jihaitao <jht1264044626@gmail.com>
2021-01-07 11:05:36 +0800

    DFS 算法，依赖顺序输出

--- src/main/java/Ch43.java
+++ src/main/java/Ch43.java
@@ -57,24 +57,75 @@ public class Ch43 {
                 }
             }
         }
+
+        // DFS 算法
+        public void topoSortByDFS() {
+            // 逆邻接表
+            LinkedList<Integer> inverseAdj[] = new LinkedList[v];
+            for (int i = 0; i < v; i++) {
+                inverseAdj[i] = new LinkedList<>();
+            }
+
+            // 通过领接表创建逆邻接表
+            for (int i = 0; i < v; i++) {
+                for (int j = 0; j < adj[i].size(); j++) {
+                    int w = adj[i].get(j); // i->w
+                    inverseAdj[w].add(i);// w->i
+                }
+            }
+
+            // 记录顶点访问标记
+            boolean[] visited = new boolean[v];
+            for (int i = 0; i < v; i++) {
+                if (!visited[i]) {
+                    // 标记访问标记
+                    visited[i] = true;
+                    // 深度优先搜索
+                    dfs(i, inverseAdj, visited);
+                }
+            }
+        }
+
+        public void dfs(int vertext, LinkedList<Integer> inverseAdj[], boolean[] visited) {
+            // 遍历逆邻接表
+            for (int i = 0; i < inverseAdj[vertext].size(); i++) {
+                // w->i
+                int w = inverseAdj[vertext].get(i);
+                if (visited[w]) {
+                    continue;
+                }
+                visited[w] = true;
+                // 递归访问前驱顶点
+                dfs(w, inverseAdj, visited);
+            }
+            System.out.print("->" + vertext);
+        }
+
     }
+
 
     public static void main(String[] args) {
 
-        // 0->2->4
-        // 1->3->5
-        // 6->3
-
         Graph graph = new Graph(7);
+
+        // 0->2->4
         graph.addEdge(0, 2);
         graph.addEdge(2, 4);
 
+        // 1->3->5
         graph.addEdge(1, 3);
         graph.addEdge(3, 5);
 
+        // 6->3
         graph.addEdge(6, 3);
 
+        // Kahn算法 测试
         graph.topoSortByKahn();
+
+        System.out.println();
+
+        // DFS算法 测试
+        graph.topoSortByDFS();
 
     }
 
